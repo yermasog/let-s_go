ffdd50f39aae80bb590d6608280a69b926e7c408
ymasog <yer.masog@gmail.com>
2021-04-02 13:41:45 -0500

    centered heading, search box, and button

--- src/components/Search/Searchbar.js
+++ src/components/Search/Searchbar.js
@@ -1,17 +1,31 @@
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import "./Searchbar.css"
 
 function Searchbar() {
+
     return (
         <>
-            <h1>let's go!</h1>
-            <form noValidate autoComplete="off">
-                <TextField id="outlined-basic" label="search for a state" variant="filled" />
-            </form>
-
-            <Button variant="contained" color="primary">
-                let's go!
-            </Button>
+            <div className="grid-container">
+                <h1 className="center margin">let's go!</h1>
+                <div className="opaque">
+                <form noValidate autoComplete="off">
+                    <TextField 
+                    id="outlined-basic" 
+                    label="search for a state" 
+                    variant="filled"
+                    color="primary"
+                    fullWidth
+                    />
+                </form>
+                </div>
+                <br />
+                <div className="center">
+                <Button variant="contained" color="primary">
+                    let's go!
+                </Button>
+                </div>
+            </div>
         </>
     )
 
