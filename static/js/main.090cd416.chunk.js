6d83c32939f94a0203c457ab4217966ce14d6ab3
ymasog <yer.masog@gmail.com>
2021-04-02 11:07:20 -0500

    Updates

---
+++ static/js/main.090cd416.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplets-go"]=this["webpackJsonplets-go"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),o=n(3),s=n.n(o),i=(n(8),n(0));var j=function(){return Object(i.jsx)("p",{children:"Hello World!"})};var d=function(){return Object(i.jsx)("div",{children:Object(i.jsx)(j,{})})};s.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(d,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.090cd416.chunk.js.map
