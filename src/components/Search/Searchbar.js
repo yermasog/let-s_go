e39c826aa52cef59ab3e47b8c98fc3f0d8539371
ymasog <yer.masog@gmail.com>
2021-04-02 15:25:03 -0500

    search page layout done

--- src/components/Search/Searchbar.js
+++ src/components/Search/Searchbar.js
@@ -1,4 +1,3 @@
-import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import "./Searchbar.css"
 
@@ -7,23 +6,19 @@ function Searchbar() {
     return (
         <>
             <div className="grid-container">
-                <h1 className="center margin">let's go!</h1>
                 <div className="opaque">
-                <form noValidate autoComplete="off">
-                    <TextField 
-                    id="outlined-basic" 
-                    label="search for a state" 
-                    variant="filled"
-                    color="primary"
-                    fullWidth
-                    />
-                </form>
+                    <h1 className="center margin">Find Your Next National Park Adventure</h1>
+                    <form noValidate autoComplete="off">
+                        <TextField
+                            id="outlined-basic"
+                            label="search for a state"
+                            variant="filled"
+                            fullWidth
+                        />
+                    </form>
                 </div>
-                <br />
                 <div className="center">
-                <Button variant="contained" color="primary">
-                    let's go!
-                </Button>
+                    <button className="clear button mybutton success">Let's go!</button>
                 </div>
             </div>
         </>
