64a9cfd2a413c10bd0464995f593eea228c5a673
ymasog <yer.masog@gmail.com>
2021-04-15 15:15:39 -0500

    state page layout

--- src/pages/states.js
+++ src/pages/states.js
@@ -5,7 +5,37 @@ import React from "react";
 function States() {
    return (
       <>
-         <h1>hello world</h1>
+      <div className="grid-container">
+         <form>
+            <input
+            className="opaque"
+            type="text"
+            id="state"
+            name="state"
+            value=""
+            //  onChange={handleInputChange}
+            placeholder="Search for a State"
+            />
+                       
+            <button 
+            //  onClick={}
+            className="button success">Let's go!</button>
+         </form>
+         <div>
+            state name: hero section 
+         </div>
+         <div>
+            sort/filter
+         </div>
+         <div>
+            <div>
+               image
+            </div>
+            <div>
+               description 
+            </div>
+         </div>
+         </div>
       </>
    );
 }
