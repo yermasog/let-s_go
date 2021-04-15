4d783bef0567be7501b04be18763cf736a76d12f
ymasog <yer.masog@gmail.com>
2021-04-15 11:57:15 -0500

    search submit button sets state

--- src/components/Search/Searchbar.js
+++ src/components/Search/Searchbar.js
@@ -1,41 +1,40 @@
-import TextField from '@material-ui/core/TextField';
+// import TextField from '@material-ui/core/TextField';
 import React, { useState } from "react";
 import "./Searchbar.css"
 
 function Searchbar() {
 
-    const [searchState, setSearchState] = useState()
+    const [searchState, setSearchState] = useState("")
 
     const handleInputChange = event => {
-        const value = event.target.value;
-        setSearchState(value)
+        setSearchState(event.target.value)
+        console.log(event.target.value);
     }
 
     const handleSubmit = event => {
         event.preventDefault();
         setSearchState(event.target.value)
-        console.log(event.target.value);
+        console.log(searchState);
     }
 
     return (
         <>
             <div className="grid-container">
-                <div className="opaque">
-                    <h1 className="center margin">Start Your Next National Park Adventure</h1>
-                    <form noValidate autoComplete="off">
-                        <TextField
-                            id="state"
-                            label="state"
-                            variant="filled"
-                            value={searchState}
-                            fullWidth
-                            onChange={handleInputChange}
+                    <h1 className="center margin opaque">Start Your Next National Park Adventure</h1>
+                    <form>
+                        <input 
+                        className="opaque"
+                        type="text" 
+                        id="state" 
+                        name="state" 
+                        value={searchState} 
+                        onChange={handleInputChange}
+                        placeholder="Search for a State"
                         />
-                    </form>
-                </div>
-                <div className="center">
-                    <button onClick={handleSubmit.bind(searchState)} className="clear button mybutton success">Let's go!</button>
-                </div>
+                        <div className="center non-opaque">
+                            <button onClick={handleSubmit} className="clear button mybutton success">Let's go!</button>
+                        </div> 
+                    </form> 
             </div>
         </>
     )
